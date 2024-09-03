ebe66af2d762189945ee4faf2da4d20aa527de26
CesarBaquiro <cmbm.kf@gmail.com>
2024-09-03 01:59:13 -0500

    Add nfc reader and writer base

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,313 @@
+import 'dart:async';
+import 'dart:io' show Platform, sleep;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
+import 'package:logging/logging.dart';
+import 'package:ndef/ndef.dart' as ndef;
+import 'package:ndef/utilities.dart';
+
+import 'ndef_record/raw_record_setting.dart';
+import 'ndef_record/text_record_setting.dart';
+import 'ndef_record/uri_record_setting.dart';
 
 void main() {
-  runApp(const MyApp());
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
+  runApp(MaterialApp(
+      //debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  String _platformVersion = '';
+  NFCAvailability _availability = NFCAvailability.not_supported;
+  NFCTag? _tag;
+  String? _result, _writeResult, _mifareResult;
+  late TabController _tabController;
+  List<ndef.NDEFRecord>? _records;
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'NFC basic app',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 3, 0, 197)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'NFC basic app'),
-    );
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    if (!kIsWeb)
+      _platformVersion =
+          '${Platform.operatingSystem} ${Platform.operatingSystemVersion}';
+    else
+      _platformVersion = 'Web';
+    initPlatformState();
+    _tabController = new TabController(length: 2, vsync: this);
+    _records = [];
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    NFCAvailability availability;
+    try {
+      availability = await FlutterNfcKit.nfcAvailability;
+    } on PlatformException {
+      availability = NFCAvailability.not_supported;
+    }
 
-  void _incrementCounter() {
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
     setState(() {
-      _counter++;
+      // _platformVersion = platformVersion;
+      _availability = availability;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'TAPS',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('NFC basic app'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(text: 'Leer'),
+                Tab(text: 'Escribir'),
+              ],
+              controller: _tabController,
+            )),
+        body: new TabBarView(controller: _tabController, children: <Widget>[
+          Scrollbar(
+              child: SingleChildScrollView(
+                  child: Center(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                const SizedBox(height: 20),
+                Text(
+                    'Corriendo app en: $_platformVersion\nNFC: $_availability'),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      NFCTag tag = await FlutterNfcKit.poll();
+                      setState(() {
+                        _tag = tag;
+                      });
+                      await FlutterNfcKit.setIosAlertMessage(
+                          "Working on it...");
+                      _mifareResult = null;
+                      if (tag.standard == "ISO 14443-4 (Type B)") {
+                        String result1 =
+                            await FlutterNfcKit.transceive("00B0950000");
+                        String result2 = await FlutterNfcKit.transceive(
+                            "00A4040009A00000000386980701");
+                        setState(() {
+                          _result = '1: $result1\n2: $result2\n';
+                        });
+                      } else if (tag.type == NFCTagType.iso18092) {
+                        String result1 =
+                            await FlutterNfcKit.transceive("060080080100");
+                        setState(() {
+                          _result = '1: $result1\n';
+                        });
+                      } else if (tag.ndefAvailable ?? false) {
+                        var ndefRecords = await FlutterNfcKit.readNDEFRecords();
+                        var ndefString = '';
+                        for (int i = 0; i < ndefRecords.length; i++) {
+                          ndefString += '${i + 1}: ${ndefRecords[i]}\n';
+                        }
+                        setState(() {
+                          _result = ndefString;
+                        });
+                      } else if (tag.type == NFCTagType.webusb) {
+                        var r = await FlutterNfcKit.transceive(
+                            "00A4040006D27600012401");
+                        print(r);
+                      }
+                    } catch (e) {
+                      setState(() {
+                        _result = 'error: $e';
+                      });
+                    }
+
+                    // Pretend that we are working
+                    if (!kIsWeb) sleep(new Duration(seconds: 1));
+                    await FlutterNfcKit.finish(iosAlertMessage: "Finished!");
+                  },
+                  child: Text('Escanear'),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: _tag != null
+                        ? Text(
+                            'ID: ${_tag!.id}\nStandard: ${_tag!.standard}\nType: ${_tag!.type}\nATQA: ${_tag!.atqa}\nSAK: ${_tag!.sak}\nHistorical Bytes: ${_tag!.historicalBytes}\nProtocol Info: ${_tag!.protocolInfo}\nApplication Data: ${_tag!.applicationData}\nHigher Layer Response: ${_tag!.hiLayerResponse}\nManufacturer: ${_tag!.manufacturer}\nSystem Code: ${_tag!.systemCode}\nDSF ID: ${_tag!.dsfId}\nNDEF Available: ${_tag!.ndefAvailable}\nNDEF Type: ${_tag!.ndefType}\nNDEF Writable: ${_tag!.ndefWritable}\nNDEF Can Make Read Only: ${_tag!.ndefCanMakeReadOnly}\nNDEF Capacity: ${_tag!.ndefCapacity}\nMifare Info:${_tag!.mifareInfo} Transceive Result:\n$_result\n\nBlock Message:\n$_mifareResult')
+                        : const Text('No hay Chips escaneados')),
+              ])))),
+          Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () async {
+                          if (_records!.length != 0) {
+                            try {
+                              NFCTag tag = await FlutterNfcKit.poll();
+                              setState(() {
+                                _tag = tag;
+                              });
+                              if (tag.type == NFCTagType.mifare_ultralight ||
+                                  tag.type == NFCTagType.mifare_classic ||
+                                  tag.type == NFCTagType.iso15693) {
+                                await FlutterNfcKit.writeNDEFRecords(_records!);
+                                setState(() {
+                                  _writeResult = 'Se registro!';
+                                });
+                              } else {
+                                setState(() {
+                                  _writeResult =
+                                      'error: NDEF no soportado: ${tag.type}';
+                                });
+                              }
+                            } catch (e, stacktrace) {
+                              setState(() {
+                                _writeResult = 'error: $e';
+                              });
+                              print(stacktrace);
+                            } finally {
+                              await FlutterNfcKit.finish();
+                            }
+                          } else {
+                            setState(() {
+                              _writeResult = 'error: No registrado';
+                            });
+                          }
+                        },
+                        child: Text("Empezar a escribir"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return SimpleDialog(
+                                    title: Text("Tipo de registro"),
+                                    children: <Widget>[
+                                      SimpleDialogOption(
+                                        child: Text("Texto registrado"),
+                                        onPressed: () async {
+                                          Navigator.pop(context);
+                                          final result = await Navigator.push(
+                                              context, MaterialPageRoute(
+                                                  builder: (context) {
+                                            return NDEFTextRecordSetting();
+                                          }));
+                                          if (result != null) {
+                                            if (result is ndef.TextRecord) {
+                                              setState(() {
+                                                _records!.add(result);
+                                              });
+                                            }
+                                          }
+                                        },
+                                      ),
+                                      SimpleDialogOption(
+                                        child: Text("Registro Uri"),
+                                        onPressed: () async {
+                                          Navigator.pop(context);
+                                          final result = await Navigator.push(
+                                              context, MaterialPageRoute(
+                                                  builder: (context) {
+                                            return NDEFUriRecordSetting();
+                                          }));
+                                          if (result != null) {
+                                            if (result is ndef.UriRecord) {
+                                              setState(() {
+                                                _records!.add(result);
+                                              });
+                                            }
+                                          }
+                                        },
+                                      ),
+                                      SimpleDialogOption(
+                                        child: Text("Registro RAW"),
+                                        onPressed: () async {
+                                          Navigator.pop(context);
+                                          final result = await Navigator.push(
+                                              context, MaterialPageRoute(
+                                                  builder: (context) {
+                                            return NDEFRecordSetting();
+                                          }));
+                                          if (result != null) {
+                                            if (result is ndef.NDEFRecord) {
+                                              setState(() {
+                                                _records!.add(result);
+                                              });
+                                            }
+                                          }
+                                        },
+                                      ),
+                                    ]);
+                              });
+                        },
+                        child: Text("Añadir registro"),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Text('Resultado: $_writeResult'),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    flex: 1,
+                    child: ListView(
+                        shrinkWrap: true,
+                        children: List<Widget>.generate(
+                            _records!.length,
+                            (index) => GestureDetector(
+                                  child: Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Text(
+                                          'id:${_records![index].idString}\ntnf:${_records![index].tnf}\ntype:${_records![index].type?.toHexString()}\npayload:${_records![index].payload?.toHexString()}\n')),
+                                  onTap: () async {
+                                    final result = await Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      return NDEFRecordSetting(
+                                          record: _records![index]);
+                                    }));
+                                    if (result != null) {
+                                      if (result is ndef.NDEFRecord) {
+                                        setState(() {
+                                          _records![index] = result;
+                                        });
+                                      } else if (result is String &&
+                                          result == "Delete") {
+                                        _records!.removeAt(index);
+                                      }
+                                    }
+                                  },
+                                ))),
+                  ),
+                ]),
+          )
+        ]),
       ),
     );
   }
